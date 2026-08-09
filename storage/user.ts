da33006cb3145f6843679d9277943da7616f201d
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2026-08-09 01:18:36 -0300

    call function for register token

--- storage/user.ts
+++ storage/user.ts
@@ -1,9 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { UserId } from "@/types";
 import { STORAGE_KEYS } from './keys';
+import { registerPushToken } from '@/lib/notifications';
 
 export async function saveActiveUser(userId: UserId): Promise<void> {
-  await AsyncStorage.setItem(STORAGE_KEYS.ACTIVE_USER, userId);
+    await AsyncStorage.setItem(STORAGE_KEYS.ACTIVE_USER, userId);
+      // Registra o token para o novo usuário ativo
+      await registerPushToken();
 }
 
 export async function loadActiveUser(): Promise<UserId | null>{
